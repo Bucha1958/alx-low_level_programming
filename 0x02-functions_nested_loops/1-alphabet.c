a941ccebb312c0d9dc6f39075eafc576d63e2436
Okorie Stanley <okoriemmadu@gmail.com>
2022-06-11 15:55:17 -0700

    Always to face it

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * print_alphabet - print alphabets in lowercase
+ *
+ * Return type : void
+ *
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	return;
+}
