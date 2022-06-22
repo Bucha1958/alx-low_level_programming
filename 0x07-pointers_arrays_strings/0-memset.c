e4eb531f17d69d1bd6e00666640aec7e222db8d1
Okorie Stanley <okoriemmadu@gmail.com>
2022-06-22 04:17:45 -0700

    My first pointers commit

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memset - fills memory with constant byte
+ * @s : array to set
+ * @b : value to set it as
+ * @n : how many times it is going to print
+ * Return : s variable
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
