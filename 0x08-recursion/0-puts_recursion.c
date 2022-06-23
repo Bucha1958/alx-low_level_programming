e5066b4f1a1e682139cec71d7e51c03edca772af
Okorie Stanley <okoriemmadu@gmail.com>
2022-06-23 06:21:12 -0700

    Include recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - function that print a string
+ *
+ * Return : always return 0
+ *
+ */
+int main(void)
+{
+	puts("Printnew");
+	puts('\n');
+	return (0);
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,15 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+/**
+ * _putchar - writes the character c to std
+ * @c: parameter to compare
+ * Return: 1 or 0
+ */
+int _putchar(char c);
+/**
+ * _puts_recursion - function that print a string
+ *
+ * @s : operand s
+ */
+void _puts_recursion(char *s);
+#endif
