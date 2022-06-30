14882e66bf38501c5258f19f217a2d82b65dd15a
Okorie Stanley <okoriemmadu@gmail.com>
2022-06-29 18:46:12 -0700

    Fine

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * main - entry point
+ * main - main function
  * @argc : number of arguments passed in the command line
  * @argv : pointer array of string
  * Returns : always 0
- *
+ * int : it is the return type of main
  */
 int main(int argc, __attribute__((unused))char *argv[])
 {
