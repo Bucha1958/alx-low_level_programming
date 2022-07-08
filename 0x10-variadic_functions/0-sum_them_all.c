8fb8fce102ab357669210d4886cb3f64ec422203
Okorie Stanley <okoriemmadu@gmail.com>
2022-07-08 05:30:56 -0700

    Push again

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,9 +12,14 @@ int sum_them_all(const unsigned int n, ...)
 {
 	va_list ap;
 
+	if (n == 0)
+	{
+		return (0);
+	}
+
 	va_start(ap, n);
 
-	int i, add, sum;
+	int i, add, sum = 0;
 
 	for (i = 0; i < n; i++)
 	{
