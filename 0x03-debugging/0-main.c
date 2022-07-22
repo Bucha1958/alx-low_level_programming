f5bb3e771fa60dc122b1dd69ab17593ede58ab96
Okorie Stanley <okoriemmadu@gmail.com>
2022-07-22 08:25:44 -0700

    Debugging file

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * 
+ * main - Function that checkes whether an integar is positive or negative 
+ *
+ * Return : Always returns 0
+ *
+ */
+int main(void)
+{
+	positive_or_negative(98);
+	return (0);
+}
