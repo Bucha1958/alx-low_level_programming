a2126974ae02a4ab4bf6d93cce0b3fc387dfe354
Okorie Stanley <okoriemmadu@gmail.com>
2022-07-29 03:54:20 -0700

    Modification

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include "2-strlen.c"
 /**
  * _atoi - converts string to integer
  * @s: string to convert
@@ -8,38 +7,22 @@
  */
 int _atoi(char *s)
 {
-	int i;
-	int np = 0;
-	int c;
-	int d = 1;
-	int num = 0;
+	int sign;
+	int res;
 
-	for (i = 0; i < _strlen(s); i++)
+	sign = 1;
+	res = 0;
+	while (*s == 32 || (*s >= 9 && *s <= 13))
+		s++;
+	if (*s == '-')
+		sign = sign * -1;
+	if (*s == '-' || *s == '+')
+		s++;
+	while (*s >= '0' && *s <= '9')
 	{
-		if (!(s[i] >= '0' && s[i] <= '9') && c > 0)
-			break;
-		if (s[i] == '-')
-			np--;
-		if (s[i] == '+')
-			np++;
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			c++;
-		}
+		res = res * 10 + *s - '0';
+		s++;
 	}
-	while (c > 0)
-	{
-		num += ((s[i - 1] - '0') * d);
-		i--;
-		c--;
-		d *= 10;
-	}
-	if (np >= 0)
-	{
-		num *= 1;
-	} else
-	{
-		num *= -1;
-	}
-	return (num);
+	return (res * sign);
 }
+
