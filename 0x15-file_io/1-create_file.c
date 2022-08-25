3ae55df72bbed3d3a28b358c347a37c20e1385be
Okorie Stanley <okoriemmadu@gmail.com>
2022-08-25 10:01:07 -0700

    Test again

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,30 +1,37 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * create_file - creates a file
- * @filename: filename to grab
- * @text_content: content to add
- * Return: int value
+ * create_file - Function that creates a file.
+ * @filename: The name of the file to create.
+ * @text_content: Is a string to write to the file.
+ * Return: 1 on success, -1 on failure.
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, len = 0;
-	long int wrote;
+	int fd;
+	int i;
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	if (fd == -1)
-		return (-1);
-	if (text_content != NULL)
+	if (text_content == NULL)
+		text_content = "";
+	fd = open(filename, O_CREAT | O_EXCL | O_WRONLY, 0600);
+	if (fd < 0)
 	{
-		while (text_content[len])
-			len++;
-		wrote = write(fd, text_content, len);
-		if (wrote == -1)
+		if (errno == EEXIST)
+		{
+			fd = open(filename, O_WRONLY | O_TRUNC);
+			if (fd == -1)
+				return (-1);
+		}
+		else
 			return (-1);
 	}
-	if (close(fd) == -1)
-		return (-1);
+	for (i = 0; text_content[i] != '\0'; i++)
+	{
+		if (write(fd, &text_content[i], 1) == -1)
+			return (-1);
+	}
+	close(fd);
 	return (1);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,7 +5,9 @@
 #include <sys/stat.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <errno.h>
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 #endif
