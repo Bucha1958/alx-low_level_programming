2dc91dff8420d978e96ab7a96e9b6cb162405737
Bucha1958 <okoriemmadu@gmail.com>
2022-09-18 10:17:30 +0000

    Added files

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
+ * factorial - returns the factorial of a given number.
  *
- *
- *
- *
+ * @n : number.
+ * Return : -1 if n is lower than 0, return factorial otherwise.
  *
  */
 int factorial(int n)
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -19,5 +19,12 @@ void _puts_recursion(char *s);
  * Return : it returns 1
  */
 int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int isqrt(int n, int i);
+int is_prime(int n, int i);
 #endif
 
