006376cdbe8630854fd1f3ffab0f06e0865ca38b
Bucha1958 <okoriemmadu@gmail.com>
2022-09-18 10:24:33 +0000

    Try again

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,11 +8,11 @@
  */
 int factorial(int n)
 {
-	if (n == NULL)
+	if (n < 0)
 	{
-		return (0);
-	} else
-	{
-		return (n * factorial(n - 1));
+		return (-1);
 	}
+	if (n <= 1)
+		return (1);
+	return (n * factorial(n - 1));
 }
