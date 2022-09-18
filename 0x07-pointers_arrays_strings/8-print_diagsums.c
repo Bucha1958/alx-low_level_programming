76668a589e47d78817d5d606dfa841a9334841b4
Bucha1958 <okoriemmadu@gmail.com>
2022-09-18 08:25:36 +0000

    Added files

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_diagsums - Adds numbers in a diagonal
+ * @a : Array containing characters
+ * @size : Size of array levels
+ */
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int sumR;
+	int sumL;
+
+	sumR = 0;
+	sumL = 0;
+	for (i = 0; i < (size * size);)
+	{
+		sumR = sumR + a[i];
+		i = i + size + 1;
+	}
+	for (i = 0; i < (size * size) - size;)
+	{
+		i = i + size - 1;
+		sumL = sumL + a[i];
+	}
+	printf("%d, %d\n", sumR, sumL);
+}
